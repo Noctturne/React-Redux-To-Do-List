45468714659f367b172172a5db248d656e692d97
Noctturne <88271478+Noctturne@users.noreply.github.com>
2021-08-25 18:45:48 +0200

    Creación del Header

---
+++ todo/src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className="navbar bg-dark p-3">
+            <div className="container-fluid">
+                <h1 className="navbar-brand mb-0 text-blue"> <i class="fas fa-cat text-blue"></i> To-Do </h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
