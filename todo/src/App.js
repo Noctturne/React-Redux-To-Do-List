23cc22859fd92a467aa7dd2f8fe505f3015e9842
Noctturne <88271478+Noctturne@users.noreply.github.com>
2021-08-26 10:37:13 +0200

    Configurar Redux e instalar devtools extension

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,16 +5,22 @@ import FormTask from './components/Tasks/FormTask';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 function App() {
   return (
       <Router>
-        <Header/>
-        <div className="container-fluid p-0 m-0">
-          <Switch>
-            <Route exact path="/" component={Tasks}/>
-            <Route exact path="/tasks/new" component={FormTask}/>
-          </Switch>
-        </div>
+        <Provider store={store}>
+            <Header/>
+            <div className="container-fluid p-0 m-0">
+              <Switch>
+                <Route exact path="/" component={Tasks}/>
+                <Route exact path="/tasks/new" component={FormTask}/>
+              </Switch>
+            </div>
+        </Provider>
       </Router>
   );
 }
