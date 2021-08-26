7468621e312f6d296c7ca1720116de13ae13f7b8
Noctturne <88271478+Noctturne@users.noreply.github.com>
2021-08-26 11:47:18 +0200

    Creando las acciones para añadir tarea

--- todo/src/redux/actions/taskActions.js
+++ todo/src/redux/actions/taskActions.js
@@ -3,7 +3,29 @@ import { ADD_TASK, ADD_TASK_SUCCESS, ADD_TASK_ERROR } from '../types';
 
 // Crear tareas
 export function createTaskRedux(task){
-    return () => {
-        console.log(task);
+    return (dispatch) => {
+        dispatch( addTaskAPI() );
+
+        try {
+            dispatch( addTaskAPISuccess(task) );
+        } catch (e) {
+            console.log(e);
+            dispatch ( addTaskAPIError(true) );
+        }
     }
 }
+
+const addTaskAPI = () => ({
+    type: ADD_TASK,
+    payload: true
+});
+
+const addTaskAPISuccess = task => ({
+    type: ADD_TASK_SUCCESS,
+    payload: task
+});
+
+const addTaskAPIError = state => ({
+    type: ADD_TASK_ERROR,
+    payload: state
+});
--- todo/src/redux/reducers/tasksReducer.js
+++ todo/src/redux/reducers/tasksReducer.js
@@ -9,6 +9,23 @@ const initialState = {
 
 export default function(state = initialState, action){
     switch(action.type){
+        case ADD_TASK:
+            return{
+                ...state,
+                loading: action.payload
+            }
+        case ADD_TASK_SUCCESS:
+            return{
+                ...state,
+                loading: false,
+                tasks: [...state.tasks, action.payload]
+            }
+        case ADD_TASK_ERROR:
+            return{
+                ...state,
+                loading: false,
+                error: action.payload
+            }
         default:
             return state;
     }
